3230c706cc9137911852d268111f1547e704c8fb
purnachandra238 <purnachandra.settipalli@gmail.com>
2025-07-29 10:54:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use("/api", studentRoutes);
+app.use("/", studentRoutes);
 
 const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
